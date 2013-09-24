caefeb0067157dff832766ecca7f189d8afda2fb
John Chilton <jmchilton@gmail.com>
2013-09-23 21:39:28 -0500

    Give john sudo access.

--- manifests/sitedefs.pp
+++ manifests/sitedefs.pp
@@ -1,10 +1,21 @@
 
 class dev_machine {
+  include base
 
   package {'git':
   }
 
   package {'mercurial':
+  }
+
+}
+
+class base {
+  
+  class { 'sudo': }
+
+  sudo::conf { 'john':
+    content => 'john    ALL=(ALL:ALL) ALL',
   }
 
 }
